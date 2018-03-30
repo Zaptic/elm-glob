a8896d7b956d1fc8613fc8cb771cc8b6979693b7
Michael Jones <michael@zaptic.com>
2018-03-30 22:08:07 +0100

    Add exposed modules & empty docs
    Still getting a feel for how documentation & module prep works.

--- src/Glob.elm
+++ src/Glob.elm
@@ -1,23 +1,33 @@
-module Glob exposing (Glob, Options, defaultOptions, glob, globWithOptions, toRegexString)
+module Glob exposing (Error, Glob, Options, caseInsensitive, defaultOptions, glob, globWithOptions, toRegexString)
+
+{-|
+
+@docs Glob, Error, glob, globWithOptions, caseInsensitive, Options, defaultOptions, toRegexString
+
+-}
 
 import Glob.Core exposing (..)
 import Parser
 import Regex
 
 
+{-| -}
 type Glob
     = Glob Regex.Regex
 
 
+{-| -}
 type Error
     = ParserError Parser.Error
 
 
+{-| -}
 glob : String -> Result Error Glob
 glob string =
     globWithOptions defaultOptions string
 
 
+{-| -}
 globWithOptions : Options -> String -> Result Error Glob
 globWithOptions options string =
     parseWithOptions options string
@@ -25,12 +35,14 @@ globWithOptions options string =
         |> Result.map (renderRegexString >> Regex.regex >> Glob)
 
 
+{-| -}
 caseInsensitive : Glob -> Glob
 caseInsensitive (Glob regex) =
     Regex.caseInsensitive regex
         |> Glob
 
 
+{-| -}
 toRegexString : Options -> String -> Result Error String
 toRegexString options pattern =
     parseWithOptions options pattern
@@ -38,6 +50,7 @@ toRegexString options pattern =
         |> Result.map renderRegexString
 
 
+{-| -}
 type alias Options =
     { enableAsterisk : Bool
     , enableQuestionMark : Bool
@@ -45,6 +58,7 @@ type alias Options =
     }
 
 
+{-| -}
 defaultOptions : Options
 defaultOptions =
     { enableAsterisk = True
